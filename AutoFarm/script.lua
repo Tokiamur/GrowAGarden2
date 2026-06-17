5b0752f6789a0aa3c5a5a6b817c9be0c1c212446
Tokiamur <aporhyaunpai@gmail.com>
2026-06-17 20:22:20 +0300

    Update script.lua

--- AutoFarm/script.lua
+++ AutoFarm/script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjAwNGFkODZkLTFmNGMtNDZiZS1iZWQ1LWYxOTUyZjAxZTFjYSIsImtpbmQiOiJtZWRpYSIsInZpc3VhbCI6eyJpZCI6ImdhZzJfbWFpbl92aXN1YWwifX0"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6IjAwNGFkODZkLTFmNGMtNDZiZS1iZWQ1LWYxOTUyZjAxZTFjYSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJnYWcyX21haW5fdmlzdWFsIn19"))()
